23c96ccf8883e11960f020640d4d4d2f0ed2283e
Felix <Felix@LAPTOP-5FVQ6FDE>
2026-05-07 20:04:13 +0800

    FEAT: Logout button on all admin pages, Player Login Page, Player Dashboard Page, Top up balance screen.

---
+++ ELNETFINALPROJECT/Controllers/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELNETFINALPROJECT.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // login logic here
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
